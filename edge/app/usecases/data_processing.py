3a0467eca7ab15a2beb7cc551df1debc8b7366a8
invisy <3dvlad2013@gmail.com>
2024-06-14 12:57:29 +0300

    Update pit detector

--- edge/app/usecases/data_processing.py
+++ edge/app/usecases/data_processing.py
@@ -12,4 +12,11 @@ def process_agent_data(
     Returns:
         processed_data_batch (ProcessedAgentData): Processed data containing the classified state of the road surface and agent data.
     """
-    # Implement it
+    if agent_data.accelerometer.y > 3000:
+        road_state = "bump"
+    elif agent_data.accelerometer.y < -2000:
+        road_state = "pothole"
+    else:
+        road_state = "smooth"
+
+    return ProcessedAgentData(road_state=road_state, agent_data=agent_data)
