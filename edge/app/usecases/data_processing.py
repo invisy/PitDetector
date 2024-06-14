976e29bfaf48a7ce01d5555b9fbf7315bdff2699
invisy <3dvlad2013@gmail.com>
2024-06-14 16:39:15 +0300

    Improve detection limits

--- edge/app/usecases/data_processing.py
+++ edge/app/usecases/data_processing.py
@@ -12,9 +12,9 @@ def process_agent_data(
     Returns:
         processed_data_batch (ProcessedAgentData): Processed data containing the classified state of the road surface and agent data.
     """
-    if agent_data.accelerometer.y > 3000:
+    if agent_data.accelerometer.y > 1500:
         road_state = "bump"
-    elif agent_data.accelerometer.y < -2000:
+    elif agent_data.accelerometer.y < -1500:
         road_state = "pothole"
     else:
         road_state = "smooth"
