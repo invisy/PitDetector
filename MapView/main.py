b97c6b732b1fbf98e2915b003aa2950411631165
invisy <3dvlad2013@gmail.com>
2024-06-14 19:02:21 +0300

    Fix mapview

--- MapView/main.py
+++ MapView/main.py
@@ -12,32 +12,36 @@ class MapViewApp(App):
 
         self.datasource = Datasource()
         self.car_marker = MapMarker(source="images/car.png")
-        self.pothole_marker = MapMarker(source="images/pothole.png")
-        self.bump_marker = MapMarker(source="images/bump.png")
         self.map_layer = LineMapLayer()
+        self.pothole_markers = []
+        self.bump_markers = []
 
     def on_start(self):
         """
         Встановлює необхідні маркери, викликає функцію для оновлення мапи
         """
+        self.points_queue = list() # щоб був плавний рух
         self.mapview.add_widget(self.car_marker)
-        self.mapview.add_widget(self.pothole_marker)
-        self.mapview.add_widget(self.bump_marker)
-        Clock.schedule_interval(self.update, 1.0)  # 1 раз за секунду
+        Clock.schedule_interval(self.update, 1.0 / 12)  # 12 раз за секунду
 
     def update(self, *args):
         """
         Викликається регулярно для оновлення мапи
         """
-        new_points = self.datasource.get_new_points()
-        for point in new_points:
-            if point[2] == 'smooth':
-                self.map_layer.add_point((point[0], point[1]))
-                self.update_car_marker(point)
-            elif point[2] == 'pothole':
+        self.points_queue.extend(self.datasource.get_new_points())
+
+        try:
+            point = self.points_queue.pop(0)
+            self.map_layer.add_point((point[0], point[1]))
+            self.update_car_marker(point)
+
+            if point[2] == 'pothole':
                 self.set_pothole_marker(point)
             elif point[2] == 'bump':
                 self.set_bump_marker(point)
+        except IndexError:
+            pass
+        self.mapview.trigger_update("full")
 
     def update_car_marker(self, point):
         """
@@ -45,20 +49,37 @@ class MapViewApp(App):
         :param point: GPS координати
         """
         self.car_marker.lat, self.car_marker.lon = point[0], point[1]
+        # self.mapview.remove_widget(self.car_marker)
+        # self.mapview.add_widget(self.car_marker)
 
     def set_pothole_marker(self, point):
         """
         Встановлює маркер для ями
         :param point: GPS координати
         """
+        same_markers = [marker for marker in self.pothole_markers if marker.lat == point[0] and marker.lon == point[1]]
+        if len(same_markers) > 0:
+            return
+        self.pothole_marker = MapMarker(source="images/pothole.png")
+        self.mapview.add_widget(self.pothole_marker)
         self.pothole_marker.lat, self.pothole_marker.lon = point[0], point[1]
+        self.mapview.remove_widget(self.pothole_marker)
+        self.mapview.add_widget(self.pothole_marker)
+        self.pothole_markers.append(self.pothole_marker)
 
     def set_bump_marker(self, point):
         """
         Встановлює маркер для лежачого поліцейського
         :param point: GPS координати
         """
+        same_markers = [marker for marker in self.bump_markers if marker.lat == point[0] and marker.lon == point[1]]
+        if len(same_markers) > 0:
+            return
+
+        self.bump_marker = MapMarker(source="images/bump.png")
+        self.mapview.add_widget(self.bump_marker)
         self.bump_marker.lat, self.bump_marker.lon = point[0], point[1]
+        self.bump_markers.append(self.bump_marker)
 
     def build(self):
         """
